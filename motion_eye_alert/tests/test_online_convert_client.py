25da2b42bfe6ea0385a43227a47f597a56a06d8f
Nathan Cashmore <nathan.cashmore@staticvoid.co.uk>
2017-01-04 23:28:17 +0000

    Abstract MotionEye server URL in code and tests

--- motion_eye_alert/tests/test_online_convert_client.py
+++ motion_eye_alert/tests/test_online_convert_client.py
@@ -1,6 +1,7 @@
 import unittest
 
 import motion_eye_alert
+import motion_eye_alert.config
 from motion_eye_alert.custom_exceptions import TooManyRequests
 from motion_eye_alert.online_convert_client import OnlineConvertClient
 
@@ -10,11 +11,11 @@ class TestConvertClient(unittest.TestCase):
         self.client = motion_eye_alert.online_convert_client.OnlineConvertClient()
 
     def test_success_call(self):
-        test_file = "http://staticvoid.no-ip.info:9999/movie/1/download/integration-test/00-00-00.avi"
+        test_file = motion_eye_alert.config.MOTION_EYE_SERVER_URL + "/movie/1/download/integration-test/00-00-00.avi"
         self.client.convertFile(test_file)
 
     def test_fail_requesting_source_file(self):
-        test_file = "http://staticvoid.no-ip.info:9999/movie/1/download/integration-test/xx-xx-xx.avi"
+        test_file = motion_eye_alert.config.MOTION_EYE_SERVER_URL + "/movie/1/download/integration-test/xx-xx-xx.avi"
         self.assertRaises(TooManyRequests, self.client.convertFile, test_file)
 
 if __name__ == '__main__':
